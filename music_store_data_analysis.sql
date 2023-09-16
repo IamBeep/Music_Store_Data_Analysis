962bdd5c24d1df3dc38888c10684d32de1964922
IamBeep <ishmamfaruki443@gmail.com>
2023-09-17 00:38:22 +0600

    Update music_store_data_analysis.sql

--- music_store_data_analysis.sql
+++ music_store_data_analysis.sql
@@ -91,6 +91,60 @@ join best_selling_artist bsa on bsa.artist_id =alb.artist_id
 group by 1,2,3,4
 order by 5 desc;
 
+/* We wnat to find out the most popular music genre for each country.
+We determine the most popular genre as the genre with the highest amount of puchases.
+write a query that returns each country along with the top genre.
+For countries where the maximum numhber of purchases is shared return all genres.*/
+
+with popular_genre as(
+	select count (invoice_line.quantity) as purchases,customer.country,genre.name,genre.genre_id,
+	row_number() over (partition by customer.country order by count(invoice_line.quantity)desc)as RowNo
+	from invoice_line
+	join invoice on invoice.invoice_id=invoice_line.invoice_id
+	join customer on customer.customer_id =invoice.customer_id
+	join track on track.track_id=invoice_line.track_id
+	join genre on genre.genre_id = track.genre_id
+	group by 2,3,4
+	order by 2 asc,1 desc
+	
+)
+select * from popular_genre where RowNo<=1
+
+
+
+/*Write a query that determines the customer that has spent the most on music for each country.
+Write a query that returns the country along with the top customer and how much they spent.
+For countries where the top amount spent is shared,provide all customers who spent this amount.*/
+
+with recursive
+     customer_with_country as (select customer.customer_id,first_name,last_name,billing_country,sum(total) as total_spending
+							   from invoice
+							   join customer on customer.customer_id=invoice.customer_id
+							   group by 1,2,3,4
+							   order by 2,3 desc),
+							 
+	 country_max_spending as (
+		 select billing_country,max(total_spending) as max_spending
+		 from customer_with_country
+		 group by billing_country)
+		 
+
+select cc.billing_country,cc.total_spending,cc.first_name,cc,last_name,cc.customer_id
+from customer_with_country cc
+join country_max_spending ms
+on cc.billing_country = ms.billing_country
+where cc.total_spending = ms.max_spending
+order by 1;
+	 
+	 
+
+
+
+
+
+
+
+
 
 
 
